68e0097f7d6076a2b38e6e2c715ffecc1f88eb5b
Marcin Machalica <marcin.h.machalica@gmail.com>
2019-04-10 18:26:26 +0200

    Added QR codes for downloading files

--- src/main/java/machalica/marcin/spring/ytdownloader/downloader/YtDownloadFile.java
+++ src/main/java/machalica/marcin/spring/ytdownloader/downloader/YtDownloadFile.java
@@ -3,17 +3,23 @@ package machalica.marcin.spring.ytdownloader.downloader;
 import java.util.LinkedHashMap;
 
 public class YtDownloadFile {
-	private static final YtDownloadFile EMPTY_YT_DOWNLOAD_FILE = new YtDownloadFile(new LinkedHashMap<String, String>(), "");
+	private static final YtDownloadFile EMPTY_YT_DOWNLOAD_FILE = new YtDownloadFile(new LinkedHashMap<String, String>(), new LinkedHashMap<String, String>(), "");
 	private final LinkedHashMap<String, String> formats;
+	private final LinkedHashMap<String, String> qrCodes;
 	private final String url;
 
-	public YtDownloadFile(LinkedHashMap<String, String> formats2, String url) {
-		this.formats = formats2;
+	public YtDownloadFile(LinkedHashMap<String, String> formats, LinkedHashMap<String, String> qrCodes, String url) {
+		this.formats = formats;
+		this.qrCodes = qrCodes;
 		this.url = url;
 	}
 
 	public LinkedHashMap<String, String> getFormats() {
 		return formats;
+	}
+	
+	public LinkedHashMap<String, String> getQrCodes() {
+		return qrCodes;
 	}
 
 	public String getUrl() {
